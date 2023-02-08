1d851e3ec629fc674cf4d6d5015c32db442a40eb
Juan Pablo Mella Campos <juanp.mella@mineduc.cl>
2023-02-08 09:41:17 -0300

    Agregado db sqlite, modelo y controlador

---
+++ inventarioportable/Controlador/controladorregistro.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Security.Cryptography.X509Certificates;
+using inventarioportable.Modelo;
+using System.Data.SQLite;
+namespace inventarioportable.Controlador
+{
+    public class controladorregistro
+    {
+        //crea una variable que guardara la string de connection de la configuracion
+        private static string cdena = ConfigurationManager.ConnectionStrings["string"].ConnectionString;
+        // crea una instancia nula
+        private static controladorregistro inst = null;
+        //constructor
+        public controladorregistro()
+        {
+
+        }
+        //permite hacer instancia unica al registro
+        public static controladorregistro Instancia
+        {
+            get
+            {
+                if(inst == null)
+                {
+                    inst = new controladorregistro();
+                }
+                //retorna la instancia
+                return inst;
+            }
+        }
+        public bool insert(Registro obj)
+        {
+            bool resp = true;
+
+            using (SQLiteConnection con = new SQLiteConnection(cdena)) 
+            {
+
+            }
+
+            return resp;
+
+        }
+    }
+}
---
+++ inventarioportable/Modelo/Registro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventarioportable.Modelo
+{
+    public class Registro
+    {
+        public int inventarioid { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Rut { get; set; }
+        public string Departamento { get; set; }
+        public string Unidad { get; set; }
+        public string Tipo_de_Equipo { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Serie { get; set; }
+        public string Inventario { get; set; }
+        public string Usuario { get; set; }
+        public string Nombre_de_Equipo { get; set; }
+        public string MAC { get; set; }
+        public string RAM { get; set; }
+        public string Espacio_Disco { get; set; }
+        public string Procesador { get; set; }
+        public string Version_Windows { get; set; }
+        public string Version_Office { get; set; }
+        public string Lojack { get; set; }
+    }
+}
